7c781a4af637fbf851a61033003cac5df85c1950
nbsteuv <nbsteuv@gmail.com>
2018-05-23 07:14:21 -0400

    Restyle login and signup pages

--- TileGame.Website/src/app/authorization/authorization.component.ts
+++ TileGame.Website/src/app/authorization/authorization.component.ts
@@ -1,7 +1,22 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'authorization-root',
-  templateUrl: './authorization.component.html'
+    selector: 'authorization-root',
+    templateUrl: './authorization.component.html'
 })
-export class AuthorizationComponent {}
+export class AuthorizationComponent implements OnInit {
+
+    currentRoute: string = '';
+
+    constructor(private router: Router) { }
+
+    ngOnInit(): void {
+        this.currentRoute = this.router.url;
+    }
+
+    navigateTo(route: string): void {
+        this.router.navigate([route]);
+    }
+
+}
