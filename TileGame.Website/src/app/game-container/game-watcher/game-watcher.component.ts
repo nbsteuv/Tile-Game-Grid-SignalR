0d16cdf3102dba557abecc2d4a860c2dd3e7aed5
nbsteuv <nbsteuv@gmail.com>
2018-07-01 17:53:04 -0400

    Add watcher component files

---
+++ TileGame.Website/src/app/game-container/game-watcher/game-watcher.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'nbs-game-watcher',
+    templateUrl: './game-watcher.component.html'
+})
+export class GameWatcherComponent {
+    
+}
