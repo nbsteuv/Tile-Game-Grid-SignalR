07cc9f3fc754cf20ef6e3c0e961ea53e57d11bf3
nbsteuv <nbsteuv@gmail.com>
2018-07-01 18:36:16 -0400

    Include GameSetup model in commit

---
+++ TileGame.Business/Models/GameSetup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TileGame.Business.Models
+{
+    public class GameSetup
+    {
+        public char[] PuzzleArray { get; set; }
+        public IEnumerable<string> WordList { get; set; }
+        public IEnumerable<string> PlayerList { get; set; }
+    }
+}
