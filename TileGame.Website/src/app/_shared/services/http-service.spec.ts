00d9f24496f29091b90bae7150265d48e2fd46c6
nbsteuv <nbsteuv@gmail.com>
2018-10-08 07:26:00 -0400

    Add tests for GET and POST requests for HttpService

--- TileGame.Website/src/app/_shared/services/http-service.spec.ts
+++ TileGame.Website/src/app/_shared/services/http-service.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { HttpClient } from '@angular/common/http';
 
+import { of } from 'rxjs';
+
 import { HttpService } from './http-service';
 
+import { environment } from '../../../environments/environment';
+
 describe('HttpService', () => {
-	const mockHttpClient = {};
+	const mockHttpClient = jasmine.createSpyObj([ 'get', 'post' ]);
 	beforeEach(
 		async(() => {
 			TestBed.configureTestingModule({
@@ -16,5 +20,40 @@ describe('HttpService', () => {
 	it('should exist', () => {
 		const httpService = TestBed.get(HttpService);
 		expect(httpService).toBeTruthy();
+	});
+
+	it('should call httpClient GET with correct URL', () => {
+		// Arrange
+		mockHttpClient.get.and.returnValue(of(true));
+		const httpService = TestBed.get(HttpService);
+		const testUrl = '/test';
+
+		// Act
+		httpService.get(testUrl);
+
+		// Assert
+		const expectedUrl = environment.baseUrl + testUrl;
+		const expectedOptions = {
+			withCredentials: true
+		};
+		expect(mockHttpClient.get).toHaveBeenCalledWith(expectedUrl, expectedOptions);
+	});
+
+	it('should call httpClient POST with correct URL and body', () => {
+		// Arrange
+		mockHttpClient.post.and.returnValue(of(true));
+		const httpService = TestBed.get(HttpService);
+		const testUrl = '/test';
+		const testBody = { testProp: 'testValue' };
+
+		// Act
+		httpService.post(testUrl, testBody);
+
+		// Assert
+		const expectedUrl = environment.baseUrl + testUrl;
+		const expectedOptions = {
+			withCredentials: true
+		};
+		expect(mockHttpClient.post).toHaveBeenCalledWith(expectedUrl, testBody, expectedOptions);
 	});
 });
