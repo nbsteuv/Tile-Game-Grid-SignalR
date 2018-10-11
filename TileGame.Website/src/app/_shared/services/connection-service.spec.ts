af8c78da5645a84ae6179454abad17ad2eeec980
nbsteuv <nbsteuv@gmail.com>
2018-10-11 06:21:59 -0400

    Fixed connection service test

--- TileGame.Website/src/app/_shared/services/connection-service.spec.ts
+++ TileGame.Website/src/app/_shared/services/connection-service.spec.ts
@@ -1,39 +1,40 @@
-// import { TestBed, async } from '@angular/core/testing';
-// import { InjectionToken } from '@angular/core';
+import { TestBed, async } from '@angular/core/testing';
 
-// import { ConnectionService } from './connection-service';
+import { ConnectionService } from './connection-service';
 
-// const SIGNALR_TOKEN = new InjectionToken('signalR', {
-// 	factory: () => ''
-// });
+import { SIGNALR_TOKEN } from './signalr-provider';
 
-// function signalRFactory() {
-// 	return {
-// 		HubConnectionBuilder: () => {
-// 			return {
-// 				withUrl: (url) => {
-// 					return {
-// 						build: () => {}
-// 					};
-// 				}
-// 			};
-// 		}
-// 	};
-// }
+function signalRFactory() {
+	return {
+		HubConnectionBuilder: () => {
+			return {
+				withUrl: (url) => {
+					return {
+						build: () => {
+							return {
+								on: () => {}
+							};
+						}
+					};
+				}
+			};
+		}
+	};
+}
 
-// export const SIGNALR_PROVIDER = [ { provide: SIGNALR_TOKEN, useFactory: signalRFactory } ];
+export const SIGNALR_PROVIDER = [ { provide: SIGNALR_TOKEN, useFactory: signalRFactory } ];
 
-// describe('ConnectionService', () => {
-// 	beforeEach(
-// 		async(() => {
-// 			TestBed.configureTestingModule({
-// 				providers: [ SIGNALR_PROVIDER, ConnectionService ]
-// 			});
-// 		})
-// 	);
+describe('ConnectionService', () => {
+	beforeEach(
+		async(() => {
+			TestBed.configureTestingModule({
+				providers: [ SIGNALR_PROVIDER, ConnectionService ]
+			});
+		})
+	);
 
-// 	it('should exist', () => {
-// 		const connectionService = TestBed.get(ConnectionService);
-// 		expect(connectionService).toBeTruthy();
-// 	});
-// });
+	it('should exist', () => {
+		const connectionService = TestBed.get(ConnectionService);
+		expect(connectionService).toBeTruthy();
+	});
+});
