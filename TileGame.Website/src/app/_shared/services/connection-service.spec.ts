c49908235646b4b9a3e2194b71ecd4df7d9a3bab
nbsteuv <nbsteuv@gmail.com>
2018-10-11 06:47:00 -0400

    Fix test for ConnectionService

--- TileGame.Website/src/app/_shared/services/connection-service.spec.ts
+++ TileGame.Website/src/app/_shared/services/connection-service.spec.ts
@@ -1,39 +1,20 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { ConnectionService } from './connection-service';
-
-import { SIGNALR_TOKEN } from './signalr-provider';
-
-function signalRFactory() {
-	return {
-		HubConnectionBuilder: () => {
-			return {
-				withUrl: (url) => {
-					return {
-						build: () => {
-							return {
-								on: () => {}
-							};
-						}
-					};
-				}
-			};
-		}
-	};
-}
-
-export const SIGNALR_PROVIDER = [ { provide: SIGNALR_TOKEN, useFactory: signalRFactory } ];
+import { SignalrService } from './signalr-service';
 
 describe('ConnectionService', () => {
+	const mockSignalrService = jasmine.createSpyObj([ 'getHubConnection' ]);
 	beforeEach(
 		async(() => {
 			TestBed.configureTestingModule({
-				providers: [ SIGNALR_PROVIDER, ConnectionService ]
+				providers: [ ConnectionService, { provide: SignalrService, useValue: mockSignalrService } ]
 			});
 		})
 	);
 
 	it('should exist', () => {
+		mockSignalrService.getHubConnection.and.returnValue({ on: () => {} });
 		const connectionService = TestBed.get(ConnectionService);
 		expect(connectionService).toBeTruthy();
 	});
