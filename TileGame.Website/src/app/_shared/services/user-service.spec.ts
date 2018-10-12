3b19cc18573a14d7bffa8316cdabc47ca40917dd
nbsteuv <nbsteuv@gmail.com>
2018-10-12 07:19:27 -0400

    Complete tests with mocked UserHttpService

--- TileGame.Website/src/app/_shared/services/user-service.spec.ts
+++ TileGame.Website/src/app/_shared/services/user-service.spec.ts
@@ -3,24 +3,20 @@ import { Router } from '@angular/router';
 
 import { of, throwError } from 'rxjs';
 
-import { HttpService } from './http-service';
 import { UserHttpService } from './user-http-service';
 import { UserService } from './user-service';
 
 describe('UserService', () => {
 	const mockRouter = jasmine.createSpyObj([ 'navigate' ]);
-	const mockHttpService = jasmine.createSpyObj([ 'post' ]);
+	const mockUserHttpService = jasmine.createSpyObj([ 'checkAccess', 'register', 'login', 'logout' ]);
 	const loginUrl = '/users/login';
 	beforeEach(
 		async(() => {
-			mockRouter.navigate.calls.reset();
-			mockHttpService.post.calls.reset();
 			TestBed.configureTestingModule({
 				providers: [
 					UserService,
 					{ provide: Router, useValue: mockRouter },
-					{ provide: HttpService, useValue: mockHttpService },
-					UserHttpService
+					{ provide: UserHttpService, useValue: mockUserHttpService }
 				]
 			});
 		})
@@ -47,15 +43,13 @@ describe('UserService', () => {
 		let userService;
 		beforeEach(
 			async(() => {
-				mockRouter.navigate.calls.reset();
-				mockHttpService.post.calls.reset();
 				userService = TestBed.get(UserService);
 			})
 		);
 
 		it('should allow access to the route if check access endpoint returns true', () => {
 			// Arrange
-			mockHttpService.post.and.returnValue(of(true));
+			mockUserHttpService.checkAccess.and.returnValue(of(true));
 
 			// Act
 			const canActivate = userService.canActivate({}, { url: '/test' });
@@ -66,8 +60,7 @@ describe('UserService', () => {
 
 		it('should set isLoggedIn to true if check access endpoint returns true', () => {
 			// Arrange
-			mockHttpService.post.and.returnValue(of(true));
-
+			mockUserHttpService.checkAccess.and.returnValue(of(true));
 			// Act
 			const canActivate = userService.canActivate({}, { url: '/test' });
 
@@ -77,7 +70,8 @@ describe('UserService', () => {
 
 		it('should not redirect if check access endpoint returns true', () => {
 			// Arrange
-			mockHttpService.post.and.returnValue(of(true));
+			mockUserHttpService.checkAccess.and.returnValue(of(true));
+			mockRouter.navigate.calls.reset();
 
 			// Act
 			const canActivate = userService.canActivate({}, { url: '/test' });
@@ -88,7 +82,7 @@ describe('UserService', () => {
 
 		it('should not allow access to the route if check access endpoint returns an error', () => {
 			// Arrange
-			mockHttpService.post.and.returnValue(throwError('Test Error'));
+			mockUserHttpService.checkAccess.and.returnValue(throwError('Test Error'));
 
 			// Act
 			const canActivate = userService.canActivate({}, { url: '/test' });
@@ -99,7 +93,7 @@ describe('UserService', () => {
 
 		it('should not set isLoggedIn to true if check access endpoint returns an error', () => {
 			// Arrange
-			mockHttpService.post.and.returnValue(throwError('Test Error'));
+			mockUserHttpService.checkAccess.and.returnValue(throwError('Test Error'));
 
 			// Act
 			const canActivate = userService.canActivate({}, { url: '/test' });
@@ -110,7 +104,8 @@ describe('UserService', () => {
 
 		it('should redirect to the login route if the check access endpoint returns an error', () => {
 			// Arrange
-			mockHttpService.post.and.returnValue(throwError('Test Error'));
+			mockUserHttpService.checkAccess.and.returnValue(throwError('Test Error'));
+			mockRouter.navigate.calls.reset();
 
 			// Act
 			const canActivate = userService.canActivate({}, { url: '/test' });
@@ -124,15 +119,13 @@ describe('UserService', () => {
 		let userService;
 		beforeEach(
 			async(() => {
-				mockRouter.navigate.calls.reset();
-				mockHttpService.post.calls.reset();
 				userService = TestBed.get(UserService);
 			})
 		);
 
 		it('should set isLoggedIn to true on registration success', () => {
 			// Arrange
-			mockHttpService.post.and.returnValue(of(true));
+			mockUserHttpService.register.and.returnValue(of(true));
 			const user = { username: 'testUser' };
 
 			// Act
@@ -144,7 +137,8 @@ describe('UserService', () => {
 
 		it('should redirect to the home page on registration success', () => {
 			// Arrange
-			mockHttpService.post.and.returnValue(of(true));
+			mockUserHttpService.register.and.returnValue(of(true));
+			mockRouter.navigate.calls.reset();
 			const user = { username: 'testUser' };
 
 			// Act
@@ -156,7 +150,7 @@ describe('UserService', () => {
 
 		it('should not set isLoggedIn to true on registration failure', () => {
 			// Arrange
-			mockHttpService.post.and.returnValue(throwError('Test Error'));
+			mockUserHttpService.register.and.returnValue(throwError('Test Error'));
 			const user = { username: 'testUser' };
 
 			// Act
@@ -168,7 +162,8 @@ describe('UserService', () => {
 
 		it('should not redirect on registration failure', () => {
 			// Arrange
-			mockHttpService.post.and.returnValue(throwError('Test Error'));
+			mockUserHttpService.register.and.returnValue(throwError('Test Error'));
+			mockRouter.navigate.calls.reset();
 			const user = { username: 'testUser' };
 
 			// Act
@@ -183,15 +178,13 @@ describe('UserService', () => {
 		let userService;
 		beforeEach(
 			async(() => {
-				mockRouter.navigate.calls.reset();
-				mockHttpService.post.calls.reset();
 				userService = TestBed.get(UserService);
 			})
 		);
 
 		it('should set isLoggedIn to true on login success', () => {
 			// Arrange
-			mockHttpService.post.and.returnValue(of(true));
+			mockUserHttpService.login.and.returnValue(of(true));
 			const user = { username: 'testUser' };
 
 			// Act
@@ -203,7 +196,8 @@ describe('UserService', () => {
 
 		it('should redirect to the home page on login success', () => {
 			// Arrange
-			mockHttpService.post.and.returnValue(of(true));
+			mockUserHttpService.login.and.returnValue(of(true));
+			mockRouter.navigate.calls.reset();
 			const user = { username: 'testUser' };
 
 			// Act
@@ -215,7 +209,7 @@ describe('UserService', () => {
 
 		it('should not set isLoggedIn to true on login failure', () => {
 			// Arrange
-			mockHttpService.post.and.returnValue(throwError('Test Error'));
+			mockUserHttpService.login.and.returnValue(throwError('Test Error'));
 			const user = { username: 'testUser' };
 
 			// Act
@@ -227,7 +221,8 @@ describe('UserService', () => {
 
 		it('should not redirect on login failure', () => {
 			// Arrange
-			mockHttpService.post.and.returnValue(throwError('Test Error'));
+			mockUserHttpService.login.and.returnValue(throwError('Test Error'));
+			mockRouter.navigate.calls.reset();
 			const user = { username: 'testUser' };
 
 			// Act
@@ -242,22 +237,16 @@ describe('UserService', () => {
 		let userService;
 		beforeEach(
 			async(() => {
-				mockRouter.navigate.calls.reset();
-				mockHttpService.post.calls.reset();
-
-				mockHttpService.post.and.returnValue(of(true));
+				mockUserHttpService.login.and.returnValue(of(true));
 				userService = TestBed.get(UserService);
 				const user = { username: 'testUser' };
 				userService.login(user);
-
-				mockRouter.navigate.calls.reset();
-				mockHttpService.post.calls.reset();
 			})
 		);
 
 		it('should set isLoggedIn to false on logout success', () => {
 			// Arrange
-			mockHttpService.post.and.returnValue(of(true));
+			mockUserHttpService.logout.and.returnValue(of(true));
 
 			// Act
 			const logout = userService.logout();
@@ -268,7 +257,8 @@ describe('UserService', () => {
 
 		it('should redirect to the home page on logout success', () => {
 			// Arrange
-			mockHttpService.post.and.returnValue(of(true));
+			mockUserHttpService.logout.and.returnValue(of(true));
+			mockRouter.navigate.calls.reset();
 
 			// Act
 			const logout = userService.logout();
@@ -279,7 +269,7 @@ describe('UserService', () => {
 
 		it('should not set isLoggedIn to false on logout failure', () => {
 			// Arrange
-			mockHttpService.post.and.returnValue(throwError('Test Error'));
+			mockUserHttpService.logout.and.returnValue(throwError('Test Error'));
 
 			// Act
 			const logout = userService.logout();
@@ -290,7 +280,8 @@ describe('UserService', () => {
 
 		it('should not redirect on logout failure', () => {
 			// Arrange
-			mockHttpService.post.and.returnValue(throwError('Test Error'));
+			mockUserHttpService.logout.and.returnValue(throwError('Test Error'));
+			mockRouter.navigate.calls.reset();
 
 			// Act
 			const logout = userService.logout();
