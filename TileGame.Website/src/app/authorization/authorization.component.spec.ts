d490c0b8c38f0399e63c2e729cb581677d802beb
nbsteuv <nbsteuv@gmail.com>
2018-10-17 07:34:02 -0400

    Add routing test for authorization component

--- TileGame.Website/src/app/authorization/authorization.component.spec.ts
+++ TileGame.Website/src/app/authorization/authorization.component.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
 import { AuthorizationComponent } from './authorization.component';
 
 describe('AuthorizationComponent', () => {
+	const mockRouter = jasmine.createSpyObj([ 'navigate' ]);
 	beforeEach(
 		async(() => {
+			mockRouter.navigate.calls.reset();
 			TestBed.configureTestingModule({
-				declarations: [ AuthorizationComponent ]
+				declarations: [ AuthorizationComponent ],
+				providers: [ { provide: Router, useValue: mockRouter } ]
 			});
 		})
 	);
@@ -16,5 +20,17 @@ describe('AuthorizationComponent', () => {
 			const fixture = TestBed.createComponent(AuthorizationComponent);
 			expect(fixture.debugElement.componentInstance).toBeTruthy();
 		});
+	});
+
+	it('should call navigate on the router with the path provided', () => {
+		// Arrange
+		const component = new AuthorizationComponent(mockRouter);
+		const testPath = 'testpath';
+
+		// Act
+		component.navigateTo(testPath);
+
+		// Assert
+		expect(mockRouter.navigate).toHaveBeenCalledWith([ testPath ]);
 	});
 });
